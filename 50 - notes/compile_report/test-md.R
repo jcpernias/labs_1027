1878e8f9e6612a74bbeb0fa1aeb866699ec6945c
José C. Pernías <jcpernias@gmail.com>
2022-03-15 20:21:05 +0100

    Notes on compiling simple reports

---
+++ 50 - notes/compile_report/test-md.R
@@ -0,0 +1,31 @@
+#' Esta linea aparecerá como texto normal.
+#'
+# Sin el apóstrofe esta línea es un comentario normal.
+#'
+#' Las líneas que no empiezan con # se ejecutan con R:
+2 + 2
+
+#'
+#' # Título 1
+#'
+#' ## Título 2
+#'
+#' ### Título 3
+#'
+#' Las *palabras entre asteriscos* aparecen en cursiva.
+#'
+#' Para negrita usamos **dos asteriscos a cada lado**.
+#'
+#' Listas (hay que dejar una línea en blanco antes):
+#'
+#' - Uno.
+#' - Dos.
+#'   1. Otra.
+#'   2. Más.
+#'
+#' Tres o más asteriscos se convierten en una línea horizontal
+#' (hay que dejar una línea en blanco antes):
+#'
+#' ***
+#'
+#' Fin.
