d1c9b3ac5d1fb16d1b0ae5c8986cbab9becb3f17
José C. Pernías <jcpernias@gmail.com>
2022-05-10 10:45:39 +0200

    Add autocorrelation lab

--- 11 - traffic2/traffic2.R
+++ 11 - traffic2/traffic2.R
@@ -3,6 +3,8 @@ library(zoo)
 library(dynlm)
 library(ggplot2)
 library(car)
+library(sandwich)
+library(lmtest)
 
 db <- read_xlsx("traffic2.xlsx")
 
@@ -23,3 +25,48 @@ ts_db$prcfat <- ts_db$fatacc / ts_db$totacc * 100
 
 mod2 <- update(mod1, prcfat ~ .)
 summary(mod1)
+
+# Test Breusch-Godfrey
+#
+uhat <- resid(mod1)
+autoplot(uhat)
+
+bgaux <- update(mod1, uhat ~ . + L(uhat))
+summary(bgaux)
+
+coeftest(mod1, vcov. = vcovHAC)
+
+
+X <- coredata(model.matrix(mod1))
+y <- model.response(model.frame(mod1))
+rho <- cor(merge(uhat, lag(uhat, -1)), use = "complete.obs")[1, 2]
+qdX <- X[-1, ] -  rho * X[-NROW(X), ]
+qdY <- y - rho * lag(y, -1)
+ar1 <- lm(qdY ~ qdX - 1)
+summary(ar1)
+
+
+library(orcutt)
+library(glue)
+
+seas <- cycle(ts_db)
+the_months <- setNames(1:12, month.abb)
+sdummies <- outer(seas, the_months, "==") + 0
+colnames(sdummies) <- names(the_months)
+Z <- cbind(trend = 1:NROW(sdummies), sdummies)
+ts_db2 <- merge(ts_db,
+                zooreg(Z, start = start(ts_db), frequency = frequency(ts_db)))
+yname <- "ltotacc"
+xnames <- c("trend", month.abb[-1], "wkends", "unem", "spdlaw", "beltlaw")
+frml_str <-  as.formula(glue("{yname} ~ {paste(xnames, collapse = ' + ')}"))
+co0 <- lm(frml_str, ts_db2)
+co1 <- cochrane.orcutt(co0)
+summary(co1)
+summary(mod1)
+
+library(forecast)
+uhat <- zooreg(resid(co1)[, 1], start = c(1989, 2), frequency = 12)
+
+Acf(uhat)
+tsdiag(co1)
+
