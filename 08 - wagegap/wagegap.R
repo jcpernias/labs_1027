8ca2915d8f1b87e9627a71a32ae64c2e4d25746a
José C. Pernías <jcpernias@gmail.com>
2023-03-19 21:05:46 +0100

    Update wage gap solution

--- 08 - wagegap/wagegap.R
+++ 08 - wagegap/wagegap.R
@@ -2,49 +2,64 @@ library(lmtest)
 library(sandwich)
 library(car)
 
+esp <- read.csv("esp.csv")
 
-esp <- read.csv("esp.csv.gz")
+esp$lsalario <- log(esp$EARN)
 
-db <- data.frame(lwage = log(esp$EARNHR))
+esp$asalariado <- as.integer(!is.na(esp$lsalario))
 
-db$educ <- esp$YRSQUAL
-db$exper <- esp$C_Q09
-db$age <- esp$AGE_R
-db$female <- as.integer(esp$GENDER_R == 2)
-db$public <- as.integer(esp$D_Q03 == 2)
-db$ngo <- as.integer(esp$D_Q03 == 3)
-db$temp <- as.integer(esp$D_Q09 %in% c(2, 3))
-db$appr <- as.integer(esp$D_Q09 == 4)
-db$other <- as.integer(esp$D_Q09 %in% c(5, 6))
-db$children <- as.integer(esp$J_Q03a == 1)
-db$native <- as.integer(esp$J_Q04a == 1)
-db$partner <- as.integer(esp$J_Q02a == 1)
+esp$desempl <- as.integer(esp$C_D05 == 2)
 
-db_emp <- na.omit(db)
-mod1 <- lm(lwage ~ female, data = db_emp)
+esp$activ <- as.integer(esp$C_D05 != 3)
+
+
+esp$mujer <- as.integer(esp$GENDER_R == 2)
+
+esp$educ <- esp$YRSQUAL
+
+esp$exper <- esp$C_Q09
+
+esp$antig <- 2012 - esp$D_Q05a2
+
+esp$pub <- as.integer(esp$D_Q03 == 2)
+
+esp$ong <- as.integer(esp$D_Q03 == 3)
+
+esp$temp <- as.integer(esp$D_Q09 %in% c(2, 3))
+
+esp$apr <- as.integer(esp$D_Q09 == 4)
+
+esp$otro <- as.integer(esp$D_Q09 %in% c(5, 6))
+
+esp$edad <- esp$AGE_R
+
+esp$hijos <- as.integer(esp$J_Q03b > 0)
+
+esp$inmigr <- as.integer(esp$J_Q04a == 2)
+
+esp$pareja <- as.integer(esp$J_Q02a == 1)
+
+
+mod1 <- lm(lsalario ~ mujer, data = esp, subset = asalariado == 1)
 summary(mod1)
 
-mod2 <- update(mod1, . ~ . + educ + exper + public + ngo  +
-							 	temp + appr + other + age + children + native + partner)
-summary(mod2)
+mod2 <- update(mod1, . ~ . + educ + exper + antig +
+                 pub + ong  +
+                 temp + apr + otro + edad + hijos +
+                 inmigr + pareja)
 coeftest(mod2, vcov. = vcovHC)
+coefci(mod2, vcov. = vcovHC)
 
 
-uhat <- resid(mod2)
-uhat_sq <- uhat^2
-yhat <- fitted(mod2)
-yhat_sq <- yhat^2
-yhat_cb <- yhat^3
+mod3 <- lm(activ ~ mujer, data = esp)
+coeftest(mod3)
 
-het_aux <- lm(uhat_sq ~ yhat + yhat_sq)
-summary(het_aux)
 
-reset <- update(mod2, . ~ . + yhat_sq + yhat_cb)
-summary(reset)
-lht(reset, matchCoefs(reset, "yhat_"), vcov. = vcovHC)
+mod4 <- lm(activ ~ mujer + educ + exper +
+					 	edad + hijos + inmigr + pareja,
+					 data = esp)
+coeftest(mod4)
+coefci(mod4, vcov. = vcovHC)
 
-confint(coeftest(mod2, vcov. = vcovHC, "constant"))
 
-mod3 <- update(mod2, . ~ . + female * partner)
-coeftest(mod3, vcov. = vcovHC)
-lht(mod3, "partner + female:partner", vcov. = vcovHC)
+
