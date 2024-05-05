64fcee22e42d0e0e43456e5e308f183d0b974f64
José C. Pernías <jcpernias@gmail.com>
2024-05-05 22:09:56 +0200

    Data for traffic2 lab

---
+++ 15 - traffic2/traffic2-csv.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+library(zoo)
+
+traffic2 <- read_excel("15 - traffic2/traffic2.xlsx") |>
+  select(-fatacc) |>
+  rename(total = totacc, weekends = wkends)
+
+st <- zooreg(traffic2, start = c(1981, 1), frequency = 12)
+months <- outer(cycle(st), 1:12, \(x, y) as.integer(x == y))
+colnames(months) <- month.abb
+
+st_months <- zooreg(months, start = start(st), frequency = frequency(st))
+t <- zooreg(1:NROW(st), start = start(st), frequency = frequency(st))
+st <- merge(st, t, st_months)
+
+
+write.zoo(st, "15 - traffic2/traffic2.csv", quote = FALSE, row.names = FALSE,
+          sep = ";", dec = ",")
+
+
