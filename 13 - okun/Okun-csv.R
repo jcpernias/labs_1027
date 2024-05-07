71defcbd167c3e62500f55e037f2558d4c3b7667
José C. Pernías <jcpernias@gmail.com>
2024-05-07 10:53:18 +0200

    Remove unused variable from data file

--- 13 - okun/Okun-csv.R
+++ 13 - okun/Okun-csv.R
@@ -1,13 +1,13 @@
 library(readxl)
 library(zoo)
-library(mFilter)
+# library(mFilter)
 
-bdrems_y <- read_excel("BDREMS.xlsx",
+bdrems_y <- read_excel("13 - okun/BDREMS.xlsx",
                        sheet = "BDREMS",
                        range = "C4:C170",
                        col_names = "Y")
 
-bdrems_u <- read_excel("BDREMS.xlsx",
+bdrems_u <- read_excel("13 - okun/BDREMS.xlsx",
                        sheet = "BDREMS",
                        range = "AL4:AL170",
                        col_names = "U")
@@ -16,35 +16,8 @@ u_ts <- zooreg(bdrems_u$U, frequency = 4, start = c(1980, 1))
 ts <- merge(y = y_ts, u = u_ts) |>
   window(end = "2019-4")
 
-hp_u <- hpfilter(ts$u, freq = 1600)
-ts$un <- zooreg(hp_u$trend, start = start(ts), frequency = 4)
-write.zoo(ts, file = "okun.csv", index.name = "trimestre", row.names = FALSE,
-					sep = ";", dec = ",", quote = FALSE)
+# hp_u <- hpfilter(ts$u, freq = 1600)
+# ts$un <- zooreg(hp_u$trend, start = start(ts), frequency = 4)
 
-ts2 <- read.csv2.zoo("okun.csv", regular = TRUE, FUN = as.yearqtr)
-
-
-
-
-
-hp_ly <- hpfilter(ts$ly, freq = 1600)
-ts$lyp <- zooreg(hp_ly$trend, start = start(ts), frequency = 4)
-ts$gy <- 100 * diff(ts$ly)
-ts_year <- time(ts) |> format("%Y") |> as.integer() |>
-  zooreg(start = start(ts), frequency = 4)
-ts_qtr <- cycle(ts)
-
-ts_all <- merge(year = ts_year, qtr = ts_qtr, ts) |>
-  window(start = "1980-2")
-write.csv(ts_all,
-          file = "okun.csv", quote = FALSE,
-          row.names = FALSE)
-
-
-library(urca)
-
-st <- window(ts, end = "2019-4")
-hp_ly <- hpfilter(st$ly, freq = 1600)
-ur.df(hp_ly$cycle, type = "drift", lags = 6, selectlags = "AIC") |>
-  summary()
-plot(1:length(hp_ly$cycle), hp_ly$cycle, typ = "l")
+write.zoo(ts, file = "13 - okun/okun.csv", index.name = "trimestre",
+          row.names = FALSE, sep = ";", dec = ",", quote = FALSE)
